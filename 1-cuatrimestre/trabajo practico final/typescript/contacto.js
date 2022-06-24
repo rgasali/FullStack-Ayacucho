2305cfd14b18b6a64ffe221bc660193e75899dc7
rgasali <rgasali@gmail.com>
2022-06-24 19:29:46 -0300

    pagina completa

--- 1-cuatrimestre/trabajo practico final/typescript/contacto.js
+++ 1-cuatrimestre/trabajo practico final/typescript/contacto.js
@@ -9,4 +9,41 @@ let interesEntrenamiento = document.getElementById("interesEntrenamiento");
 let interesSuplementos = document.getElementById("interesSuplementos");
 let interesOtros = document.getElementById("interesOtros");
 let btnEnviarFormContacto = document.getElementById("btnEnviarFormContacto");
+let expRegNombreApellido = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+let expRegNumeros = /0-9/;
+let expRegCorreo = /^(([^<>()\[\] \\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@( (\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}] )|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
 
+
+
+btnEnviarFormContacto.addEventListener("click",()=>{
+    if(formNombre.value===""){
+        alert("el campo Nombre debe estar completo");
+    }
+    else if(expRegNombreApellido.test(formNombre.value)===false){
+        alert("el campo nombre solo admite letras");
+    }
+    else if(formApellido.value===""){
+        alert("el campo Apellido debe estar completo"); 
+    }
+    else if(expRegNombreApellido.test(formApellido.value)===false){
+        alert("el campo apellido solo admite letras");
+    }
+    else if(formEdad.value===""){
+        alert("el campo Edad debe estar completo"); 
+    }
+    else if(formEdad.value<13){
+        alert("usted debe ser mayor de 13 años para que podamos contactarnos")
+    }
+    else if(expRegNumeros.test(formEdad.value)===true){
+        alert("el campo edad solo admite numeros")
+    }
+    else if(formCorreo.value===""){
+        alert("el campo E-mail debe estar completo");
+    }
+    else if(expRegCorreo.test(formCorreo.value)===false){
+        alert("el correo ingresado no es un formato valido");
+    }
+    else{
+        alert("Gracias por contactarnos. a la brevedad nos estaremos comunicando contigo")
+    }
+});
--- 1-cuatrimestre/trabajo practico final/typescript/nutricion.js
+++ 1-cuatrimestre/trabajo practico final/typescript/nutricion.js
@@ -9,6 +9,7 @@ let btnCalcularMetabolismo = document.getElementById("btnCalcularMetabolismo");
 let divResultadoMetabolismo = document.getElementById("divResultadoMetabolismo");
 let metabolismo;
 let sexo;
+let expRegNumeros = /0-9/;
 
 function calcularMetabolismo(sexo,peso,altura,edad){
     if (sexo=== "varon"){
@@ -32,7 +33,26 @@ btnCalcularMetabolismo.addEventListener("click",()=>{
     else{
         sexo="" 
     }
-    calcularMetabolismo(sexo,Number(peso.value),Number(altura.value),Number(edad.value));
-    divResultadoMetabolismo.innerHTML=String(`sus calorias de mantenimiento son ${Number(metabolismo)} kcal`);
-})
+    if((expRegNumeros.test(peso)===true)||(expRegNumeros.test(altura)===true)||(expRegNumeros.test(edad)===true)){
+        divResultadoMetabolismo.innerHTML=String("solo se admiten valores numericos");
+    }
+    else if(edad.value===""){
+        divResultadoMetabolismo.innerHTML=String("el campo edad debe estar completo");
+    }
+    else if(altura.value===""){
+        divResultadoMetabolismo.innerHTML=String("el campo altura debe estar completo");
+    }
+    else if(peso.value===""){
+        divResultadoMetabolismo.innerHTML=String("el campo peso debe estar completo");
+    }
+    else if((edad.value<5)||(edad.value>89)){
+        divResultadoMetabolismo.innerHTML=String("no podemos garantizar el calculo correcto con edades menores a 5 años o mayores a 90 años") 
+    }
+    else if((altura.value<100)||(altura.value>210)){
+        divResultadoMetabolismo.innerHTML=String("no podemos garantizar el calculo correcto con alturas menores a 100 cms o mayores a 215 cms") 
+    }
+    else {calcularMetabolismo(sexo,Number(peso.value),Number(altura.value),Number(edad.value));
+        divResultadoMetabolismo.innerHTML=String(`sus calorias de mantenimiento son ${Number(metabolismo)} kcal`);
+    }
+});
 
